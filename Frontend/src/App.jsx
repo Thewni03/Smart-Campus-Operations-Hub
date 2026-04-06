a6fefb1faf53ae8d4788f89d620afb6e45125ed0
Thewni <thewni2003@gmail.com>
2026-04-06 10:28:29 +0530

    update app.jsx file with new routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,47 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import UserResource from './components/UserResource/UserResource'
-import AdminResource from './components/Adminresource/Adminresource'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/common/Navbar";
+
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import TicketListPage from "./pages/TicketListPage";
+import TicketDetailPage from "./pages/TicketDetailPage";
+import CreateTicketPage from "./pages/CreateTicketPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import UserResource from "./components/UserResource/UserResource";
+import AdminResource from "./components/Adminresource/Adminresource";
+
+const MainLayout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+);
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/resource" element={<UserResource />} />
-        <Route path="/admin-resources" element={<AdminResource />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/tickets"element={<MainLayout><TicketListPage /></MainLayout>}/>
+          <Route path="/tickets/create"element={<MainLayout><CreateTicketPage /></MainLayout>}/>
+          <Route path="/tickets/:id" element={<MainLayout><TicketDetailPage /></MainLayout>}/>
+          <Route path="/admin"element={<MainLayout> <AdminDashboardPage /></MainLayout>}/>
+          <Route path="/resource"element={ <MainLayout> <UserResource /> </MainLayout> } />
+          <Route path="/admin-resources" element={ <MainLayout> <AdminResource /> </MainLayout>}/>
+          <Route path="*" element={<MainLayout> <NotFoundPage /></MainLayout>}/>
+
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
